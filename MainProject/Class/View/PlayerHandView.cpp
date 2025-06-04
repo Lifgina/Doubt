586de8b30846c3822d2ec1da0990be41219009ab
Lifgina <ltaisa8754@gmail.com>
2025-06-04 17:22:25 +0900

    [fix]減った手札の表示がそのままになる不具合を修正

--- MainProject/Class/View/PlayerHandView.cpp
+++ MainProject/Class/View/PlayerHandView.cpp
@@ -23,14 +23,20 @@ void PlayerHandView::UpdatePlayerHands(PlayerData playerdata_)
 {
 	// プレイヤーの手札を更新
 	int playerHands = playerdata_.GetPlayerHands(); // プレイヤーの手札の枚数を取得
-	for (int i = 0; i < playerHands; i++) {
-		playerHandSprites_[i].params.pos = Math::Vector2(50.0f * i, RenderingPath->GetLogicalHeight()-200); // カードの位置を設定
+	for (int i = 0; i < 52; i++) {
 		int rank = playerdata_.GetCard(i).GetRank(); // カードのランクを取得
-		int suit = playerdata_.GetCard(i).GetSuit(); // カードのスートを取得
-		playerHandSprites_[i].params.enableDrawRect(Rectf(
-			100.0f*(rank-1), 150.0f*(suit), 100.0f, 150.0f
-		)); // カードの描画矩形を設定
-		
+		if (rank == 0) {
+			playerHandSprites_[i].params.pos = Math::Vector2(-1000.0f, 0.0f); // カードの位置を初期化
+		}
+		else {
+			playerHandSprites_[i].params.pos = Math::Vector2(50.0f * i, RenderingPath->GetLogicalHeight() - 200); // カードの位置を設定
+			int suit = playerdata_.GetCard(i).GetSuit(); // カードのスートを取得
+			playerHandSprites_[i].params.enableDrawRect(Rectf(
+				100.0f * (rank - 1), 150.0f * (suit), 100.0f, 150.0f
+			)); // カードの描画矩形を設定
+
+		}
+
 	}
 
 }
