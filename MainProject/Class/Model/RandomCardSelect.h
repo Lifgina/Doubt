7e29b3b73d0bb9e8744750da21932b0331d9be9d
Lifgina <ltaisa8754@gmail.com>
2025-06-16 14:42:47 +0900

    [fix]カードを出す際の敵AIの調整

--- MainProject/Class/Model/RandomCardSelect.h
+++ MainProject/Class/Model/RandomCardSelect.h
@@ -12,10 +12,10 @@ public:
 
 private:
 	int selectedCardIndex_[4]; // 選択されたカードのインデックスを初期化
-	float correctDiscardRate_ = 0.8; // 正しいカードをカードを出す確率
-	float discard1Rate_ = 0.5; // 1枚捨てる確率
+	float correctDiscardRate_ = 0.7; // 正しいカードをカードを出す確率
+	float discard1Rate_ = 0.4; // 1枚捨てる確率
 	float discard2Rate_ = 0.3; // 2枚捨てる確率
-	float discard3Rate_ = 0.15; // 3枚捨てる確率
+	float discard3Rate_ = 0.2; // 3枚捨てる確率
 	//4枚捨てる確率は1 - (discard1Rate_ + discard2Rate_ + discard3Rate_)
 
 };
